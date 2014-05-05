b13f311101b90100b3343cb40b5e4dc1a7305d3e
unscathed18 <unscathed21@hotmail.com>
2014-05-05 13:29:48 +0200

    Completando y redirects

--- application/controllers/Autentificacion.php
+++ application/controllers/Autentificacion.php
@@ -6,6 +6,24 @@ class Autentificacion extends CI_Controller {
     public function __construct() {
         parent::__construct();
         
+        
+        $usuario = null;
+        
+        $username = $this->session->userdata('username');
+        $pass = $this->session->userdata('pass');
+        
+        // Si existen la sesion:
+        if($username && $pass){
+                // http://ellislab.com/codeigniter/user-guide/general/urls.html
+                redirect('administracion','refresh'); // Redirect al controlador 'request'
+        }else{
+            if($this->input->post('username') && $this->input->post('pass')){
+                
+            }else{
+                $this->CI->session->set_flashdata('error_message', 'Rellene los campos!');
+                redirect('autentificacion','refresh');
+            }
+        }
         //if (session_is_valid() && isset(session_is_valid){
         //  creamos objeto Usuario con los datos de la sesion
         //  $usuario = new Usuario(sesion['username'], sesion['pass'], true);
